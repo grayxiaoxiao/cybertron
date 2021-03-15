672602e0a50e2b35ae1aa6ab7e428e16eb51c8d0
墨 <gray_xiaoniu@163.com>
2021-03-15 11:12:06 +0800

    test file

--- tests/new_command_test.go
+++ tests/new_command_test.go
@@ -5,7 +5,18 @@ import (
 	"testing"
 )
 
-func Test_CubeNewCommand(t *testing.T) {
+func Test_CubeNewWithoutArgs(t *testing.T) {
+	newCmd := transform.CmdNew
+	newCmd.Execute()
+}
+
+func Test_CubeNewWithPath(t *testing.T) {
+	newCmd := transform.CmdNew
+	newCmd.SetArgs([]string{"models/bumblebee/blank"})
+	newCmd.Execute()
+}
+
+func Test_CubeNewWithFields(t *testing.T) {
 	newCmd := transform.CmdNew
 	newCmd.SetArgs([]string{"models/bumblebee/customer", "id:int64", "name:string", "address:string", "status:int", "description", "serial_number:string", "settlement_type:int"})
 	newCmd.Execute()
